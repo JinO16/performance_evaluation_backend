bd3d08ce1ab68b07eea7ec4016409fc3cdbe584c
chenxinan <369341315@qq.com>
2020-05-05 19:53:23 +0800

    拉取前提交，提交配置文件

--- app/service/ScienceResService.js
+++ app/service/ScienceResService.js
@@ -1,4 +1,4 @@
-let TeachRes = require('../model/ScienceResMoudel').default;
+let ScienceRes = require('../model/ScienceResMoudel').default;
 let mongoose = require('mongoose');
 module.exports = {
     //添加
@@ -8,11 +8,11 @@ module.exports = {
             reqParam.name && (data.name = reqParam.name);
             reqParam.jobID && (data.jobID = reqParam.jobID);
             reqParam.station && (data.station = reqParam.station);
+           // reqParam.auditRecord && (data.auditRecord = reqParam.auditRecord);
             reqParam.auditStatus && (data.auditStatus = reqParam.auditStatus);
             reqParam.auditPerson && (data.auditPerson = reqParam.auditPerson);
             reqParam.auditTime && (data.auditTime = reqParam.auditTime);
             reqParam.auditReason && (data.auditReason = reqParam.auditReason);
-           // reqParam.teachQuality && (data.teachQuality = reqParam.teachQuality);
             reqParam.sciencePaper && (data.sciencePaper = reqParam.sciencePaper);
             reqParam.scienceProgrom && (data.scienceProgrom = reqParam.scienceProgrom);
             reqParam.scienceAward && (data.scienceAward = reqParam.scienceAward);
@@ -39,7 +39,7 @@ module.exports = {
                 if (err) {
                     reject({ code: 500, message:'获取数据失败' + err.message})
                 } else {
-                    TeachRes.find(data, null, {skip:parseInt(skip), limit: parseInt(limit)}, function (err, docs) {
+                   ScienceRes.find(data, null, {skip:parseInt(skip), limit: parseInt(limit)}, function (err, docs) {
                         if (err) {
                             reject({ code: 201, message: '获取数据失败' + err.message});
                         } else {
@@ -81,11 +81,11 @@ module.exports = {
             reqParam.name && (updateData.name = reqParam.name);
             reqParam.jobID && (updateData.jobID = reqParam.jobID);
             reqParam.station && (updateData.station = reqParam.station);
+          //  reqParam.auditRecord && (updateData.auditRecord = reqParam.auditRecord);
             reqParam.auditStatus && (updateData.auditStatus = reqParam.auditStatus);
             reqParam.auditPerson && (updateData.auditPerson = reqParam.auditPerson);
             reqParam.auditTime && (updateData.auditTime = reqParam.auditTime);
             reqParam.auditReason && (updateData.auditReason = reqParam.auditReason);
-           // reqParam.teachQuality && (updateData.teachQuality = reqParam.teachQuality);
             reqParam.sciencePaper && (updateData.sciencePaper = reqParam.sciencePaper);
             reqParam.scienceProgrom && (updateData.scienceProgrom = reqParam.scienceProgrom);
             reqParam.scienceAward && (updateData.scienceAward = reqParam.scienceAward);
@@ -95,6 +95,38 @@ module.exports = {
                     reject({ code: 500, message: '修改失败，'+ err.message});
                 } else {
                     resolve({ code: 200, message: '修改成功！'})
+                }
+            })
+        })
+    },
+     //获取所有科研考评数据单
+
+     getAllScienceRes: function (reqParam) {
+        return new Promise((resolve, reject) => {
+            let data = {}, skip, limit;
+            if (reqParam.skip) {
+                skip = reqParam.skip;
+            } else {
+                skip = 0;
+            }
+            if (reqParam.limit) {
+                limit = reqParam.limit;
+            } else {
+                limit = 10;
+            }
+            if (reqParam._id) data._id = reqParam._id;
+            if (reqParam.jobID) data.jobID = reqParam.jobID;
+           ScienceRes.countDocuments(data, function (err,count) {
+                if (err) {
+                    reject({ code: 500, message: '获取数据失败，' + err.message});
+                } else {
+                   ScienceRes.find(data, null, {skip: parseInt(skip), limit: parseInt(limit)}, function(err, docs) {
+                        if(err) {
+                            reject({ code: 201, message: '数据获取失败，'+ err.message});
+                        } else {
+                            resolve({ code: 200, count: count, message: '获取数据成功！', result:docs})
+                        }
+                    })
                 }
             })
         })
